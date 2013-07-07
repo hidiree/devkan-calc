278b2462abaaa53cbd9b93674b74b8246d179e6b
hidiree <hidiri.yayaya@gmail.com>
2013-07-07 15:24:37 +0900

    Hookテスト用のダミーコミット（戻し）

--- src/main/java/services/Calculator.java
+++ src/main/java/services/Calculator.java
@@ -13,6 +13,6 @@ public class Calculator {
     @GET
     @Path("add")
     public String add(@QueryParam("a")int a, @QueryParam("b")int b){
-        return String.valueOf(a + b + 3);
+        return String.valueOf(a + b);
     }
 }
